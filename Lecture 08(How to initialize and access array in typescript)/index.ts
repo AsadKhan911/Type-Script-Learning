3c4fb11a135a63248ae319fdbe83cc0df61acc79
Asad Khan <asadlinkinpark9@gmail.com>
2025-03-21 19:39:26 +0500

    Lecture 8-10

---
+++ Lecture 08(How to initialize and access array in typescript)/index.ts
@@ -0,0 +1,15 @@
+//method no 1 (MOST PREFERABLE)
+
+const numbers: number[] = [1,2,3,4,5]
+
+//method no 2
+
+const numbers1: number[] = new Array(1,2,3,4,5)
+
+//method no 3
+
+const names: string[] = Array.of("Asad" , "Khan")
+
+//Accessing array
+
+console.log(names[0])
